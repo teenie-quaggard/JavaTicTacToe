7955e74e6baf433c4e1466c452e3b3a5abfc32c9
Kristina Jaggard <kjaggard@8thlight.com>
2020-03-11 15:58:05 +0000

    add factory tests for null cases

--- src/tests/java/GameFactoryTest.java
+++ src/tests/java/GameFactoryTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.core.IsNot.not;
 
 
 class GameFactoryTest {
@@ -11,5 +12,12 @@ class GameFactoryTest {
         GameFactory gameFactory = new GameFactory();
         Game twoPlayerGame = gameFactory.createGame("TwoPlayerGame");
         assertThat(twoPlayerGame, instanceOf(TwoPlayerGame.class));
+    }
+
+    @Test
+    void createGameRequiresAGameType() {
+        GameFactory gameFactory = new GameFactory();
+        Game nullGame = gameFactory.createGame("Null");
+        assertThat(nullGame, not(instanceOf(TwoPlayerGame.class)));
     }
 }
--- src/tests/java/PlayerFactoryTest.java
+++ src/tests/java/PlayerFactoryTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.core.IsNot.not;
 
 class PlayerFactoryTest {
 
@@ -10,5 +11,12 @@ class PlayerFactoryTest {
         PlayerFactory playerFactory = new PlayerFactory();
         Player player = playerFactory.createPlayer("Human");
         assertThat(player, instanceOf(HumanPlayer.class));
+    }
+
+    @Test
+    void createPlayerNeedsAStringPassedToIt() {
+        PlayerFactory playerFactory = new PlayerFactory();
+        Player nullPlayer = playerFactory.createPlayer("Null");
+        assertThat(nullPlayer, not(instanceOf(HumanPlayer.class)));
     }
 }
