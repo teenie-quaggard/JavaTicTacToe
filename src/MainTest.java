98b712753f4af46a07ae0104d0fa50cd84575a7d
Kristina Jaggard <kjaggard@8thlight.com>
2020-03-11 13:43:00 +0000

    create initialise test

---
+++ src/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+
+        assertEquals(1+1, 2);
+    }
+}
