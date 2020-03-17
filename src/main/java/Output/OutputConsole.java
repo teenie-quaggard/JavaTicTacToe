b78b5fdf29a1e0e0e0335e01450e75cd1e69135e
Kristina Jaggard <kjaggard@8thlight.com>
2020-03-17 11:21:15 +0000

    create test for welcome message

--- src/main/java/Output/OutputConsole.java
+++ src/main/java/Output/OutputConsole.java
@@ -2,8 +2,10 @@ package Output;
 
 import Board.Board;
 
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class OutputConsole implements Output{
 
@@ -25,14 +27,14 @@ public class OutputConsole implements Output{
         ArrayList<String> tiles = board.getTiles();
         String boardFormat = String.format("\n %s | %s | %s \n-----------\n %s | %s | %s \n-----------\n %s | %s | %s \n\n",
                 (board.getTileContent(0)).equals("") ? "1" : board.getTileContent(0),
-                (board.getTileContent(0)).equals("") ? "2" : board.getTileContent(1),
-                (board.getTileContent(0)).equals("") ? "3" : board.getTileContent(2),
-                (board.getTileContent(0)).equals("") ? "4" : board.getTileContent(3),
-                (board.getTileContent(0)).equals("") ? "5" : board.getTileContent(4),
-                (board.getTileContent(0)).equals("") ? "6" : board.getTileContent(5),
-                (board.getTileContent(0)).equals("") ? "7" : board.getTileContent(6),
-                (board.getTileContent(0)).equals("") ? "8" : board.getTileContent(7),
-                (board.getTileContent(0)).equals("") ? "9" : board.getTileContent(9)
+                (board.getTileContent(1)).equals("") ? "2" : board.getTileContent(1),
+                (board.getTileContent(2)).equals("") ? "3" : board.getTileContent(2),
+                (board.getTileContent(3)).equals("") ? "4" : board.getTileContent(3),
+                (board.getTileContent(4)).equals("") ? "5" : board.getTileContent(4),
+                (board.getTileContent(5)).equals("") ? "6" : board.getTileContent(5),
+                (board.getTileContent(6)).equals("") ? "7" : board.getTileContent(6),
+                (board.getTileContent(7)).equals("") ? "8" : board.getTileContent(7),
+                (board.getTileContent(8)).equals("") ? "9" : board.getTileContent(8)
                 );
         output.println(boardFormat);
     };
